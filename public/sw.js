d8d8937641b693a21fae433da4dd60adad62dc36
Olexandr004 <romanyyk2004@gmail.com>
2024-10-09 18:13:42 +0300

    refactor server worker

--- public/sw.js
+++ public/sw.js
@@ -1,52 +1,24 @@
-const CACHE_NAME = 'my-cache-v1'; // Название кэша
-const OFFLINE_URL = '/offline'; // Путь к офлайн странице
+const CACHE_NAME = 'offline-cache';
+const OFFLINE_URL = '/offline';
 
-// Список файлов для кэширования
-const FILES_TO_CACHE = [
-    OFFLINE_URL,
-    // Добавьте другие ресурсы, которые хотите кэшировать
-    // Например:
-    // '/css/styles.css',
-    // '/js/script.js',
-];
-
-// Событие установки Service Worker
+// При установке Service Worker кэшируем оффлайн-страницу
 self.addEventListener('install', (event) => {
-    console.log('Service Worker установлен');
-
-    // Кэшируем файлы при установке
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            console.log('Кэширование офлайн страницы');
-            return cache.addAll(FILES_TO_CACHE);
+            return cache.addAll([OFFLINE_URL]);
         })
     );
 });
 
-// Событие активации Service Worker
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker активирован');
-
-    // Удаляем старые кэши
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        console.log('Удаляем старый кэш:', cacheName);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// Событие перехвата запросов
+// Перехватываем запросы
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => {
-            return caches.match('/offline'); // Путь к вашему компоненту
-        })
-    );
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            fetch(event.request).catch(() => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    return cache.match(OFFLINE_URL);
+                });
+            })
+        );
+    }
 });
