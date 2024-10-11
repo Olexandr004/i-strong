d171da604593cfbf7590ddcca4fbc186dda4d2b1
Olexandr004 <romanyyk2004@gmail.com>
2024-10-11 17:31:14 +0300

    fixed base-modal

--- src/shared/components/templates/base-modal/base-modal.component.tsx
+++ src/shared/components/templates/base-modal/base-modal.component.tsx
@@ -1,9 +1,6 @@
 import { AnimatePresence, motion } from 'framer-motion'
-
-import { FC, ReactNode } from 'react'
-
+import { FC, ReactNode, useEffect } from 'react'
 import { useCommonStore } from '@/shared/stores'
-
 import styles from './base-modal.module.scss'
 
 //interface
@@ -16,6 +13,21 @@ export const BaseModalComponent: FC<Readonly<IBaseModal>> = ({ children }) => {
   const isModalActive = useCommonStore((state) => state.isModalActive)
   const handleChangeCommonStore = useCommonStore((state) => state.handleChangeCommonStore)
 
+  // Закрытие модального окна при обновлении страницы
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      handleChangeCommonStore({ isModalActive: false })
+    }
+
+    // Добавляем обработчик на событие beforeunload
+    window.addEventListener('beforeunload', handleBeforeUnload)
+
+    // Удаляем обработчик при размонтировании
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload)
+    }
+  }, [handleChangeCommonStore])
+
   //return
   return (
     <AnimatePresence mode={'wait'}>
@@ -26,9 +38,6 @@ export const BaseModalComponent: FC<Readonly<IBaseModal>> = ({ children }) => {
           animate={'animate'}
           exit={'exit'}
           className={styles.modal}
-          onClick={() => {
-            handleChangeCommonStore({ isModalActive: false })
-          }}
         >
           <div className={styles.modal__box} onClick={(e) => e.stopPropagation()}>
             {children}
@@ -38,17 +47,16 @@ export const BaseModalComponent: FC<Readonly<IBaseModal>> = ({ children }) => {
     </AnimatePresence>
   )
 }
+
 export default BaseModalComponent
 
 const variants = {
   initial: {
     opacity: 0,
   },
-
   animate: {
     opacity: 1,
   },
-
   exit: {
     opacity: 0,
   },
