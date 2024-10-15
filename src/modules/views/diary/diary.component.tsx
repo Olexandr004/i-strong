3fec117a4d531817360bd42f320629d43034c9d9
Olexandr004 <romanyyk2004@gmail.com>
2024-10-15 12:03:35 +0300

    fixed bags with diary

--- src/modules/views/diary/diary.component.tsx
+++ src/modules/views/diary/diary.component.tsx
@@ -26,12 +26,10 @@ export const DiaryComponent: FC<Readonly<IDiary>> = () => {
   const token = useUserStore((state) => state.user?.access_token)
 
   const [activeTab, setActiveTab] = useState('main')
-  const [extendedBlock, setExtendedBlock] = useState<{ year: null | number; month: null | number }>(
-    {
-      year: null,
-      month: null,
-    },
-  )
+  const [extendedBlock, setExtendedBlock] = useState<{ year: number; month: number }>({
+    year: moment().year(),
+    month: moment().month() + 1, // Месяцы в moment начинаются с 0, добавляем 1
+  })
 
   const tabs = [
     { id: 'main', text: 'Основні', isActive: activeTab === 'main' },
@@ -51,13 +49,13 @@ export const DiaryComponent: FC<Readonly<IDiary>> = () => {
     isFetching,
   } = useGetRecordsByDate(
     token ?? '',
-    extendedBlock.year?.toString() ?? '',
-    extendedBlock.month?.toString() ?? '',
+    extendedBlock.year.toString(),
+    extendedBlock.month.toString(),
   )
 
   const handleRequestRecordsByDate = (year: number, month: number) => {
     if (extendedBlock.year === year && extendedBlock.month === month) {
-      setExtendedBlock({ year: null, month: null })
+      setExtendedBlock({ year: moment().year(), month: moment().month() + 1 })
     } else {
       setExtendedBlock({ year, month })
       setTimeout(() => diaryRecordsByDateRefetch(), 0)
@@ -75,14 +73,11 @@ export const DiaryComponent: FC<Readonly<IDiary>> = () => {
   }
 
   useEffect(() => {
-    // Получаем текущий месяц и год
-    const currentMonth = moment().month() - 1 // январь - 0, поэтому добавляем 1
-    const currentYear = moment().year()
-
-    // Обновляем записи
-    diaryRecordsRefetch() // обновляем записи
-    setExtendedBlock({ year: currentYear, month: currentMonth }) // устанавливаем правильный месяц
-  }, [diaryRecordsRefetch]) // Перезапускаем эффект при изменении diaryRecordsRefetch
+    // Если данные обновляются с сервера, обновляем их
+    if (diaryRecordsByDate) {
+      diaryRecordsByDateRefetch()
+    }
+  }, [diaryRecordsByDateRefetch])
 
   if (passShow) {
     return <DiaryPinCodeComponent onVerify={handlePinVerification} />
