0bff7b855e5b99719f8c01e0b82af187a4b82a5b
Olexandr004 <romanyyk2004@gmail.com>
2024-11-16 15:06:25 +0200

    is-163

--- src/shared/components/templates/modal-getting-to-instructions/modal-getting-to-instructions.component.tsx
+++ src/shared/components/templates/modal-getting-to-instructions/modal-getting-to-instructions.component.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { BaseModalComponent, ButtonComponent } from '@/shared/components'
 import styles from './modal-getting-to-instructions.module.scss'
-import { IconNextArrow, IconClose } from '@/shared/icons'
+import { IconClose } from '@/shared/icons'
 
 interface IModalGettingToInstructionsComponent {
   title: string
@@ -16,64 +16,64 @@ export const ModalGettingToInstructionsComponent: React.FC<
   IModalGettingToInstructionsComponent
 > = ({ images, buttonText, isModalActive, closeModal }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0)
-
-  // Проверка состояния images
-  useEffect(() => {
-    console.log('Received images array:', images)
-  }, [images])
-
-  const handleNextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length)
-  }
-
-  const handlePrevImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length)
-  }
-
-  const handleConfirmation = () => {
-    closeModal()
-  }
+  const [touchStartX, setTouchStartX] = useState<number | null>(null)
 
   const handleDotClick = (index: number) => {
     setCurrentImageIndex(index)
   }
 
-  console.log('Current image URL:', images[currentImageIndex])
+  const handleTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
+    setTouchStartX(event.touches[0].clientX)
+  }
 
-  const showControls = images.length > 1 // Показывать элементы управления только если картинок больше одной
+  const handleTouchMove = (event: React.TouchEvent<HTMLDivElement>) => {
+    if (!touchStartX) return
+
+    const touchEndX = event.touches[0].clientX
+    const touchDiff = touchStartX - touchEndX
+
+    if (touchDiff > 50) {
+      // Свайп влево
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length)
+      setTouchStartX(null)
+    } else if (touchDiff < -50) {
+      // Свайп вправо
+      setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length)
+      setTouchStartX(null)
+    }
+  }
+
+  const handleTouchEnd = () => {
+    setTouchStartX(null)
+  }
+
+  console.log('Current image URL:', images[currentImageIndex])
 
   return (
     <>
       {isModalActive && (
         <div className={styles.modal}>
-          <div className={styles.modal__image}>
+          <div
+            className={styles.modal__image}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+          >
             <img
               src={images[currentImageIndex]}
               alt='Guide'
               style={{ width: '100%', height: '100%', objectFit: 'contain' }}
             />
-            {showControls && (
-              <div className={styles.modal__controls}>
-                <button onClick={handlePrevImage} className={styles.modal__prev_button}>
-                  <IconNextArrow />
-                </button>
-                <button onClick={handleNextImage} className={styles.modal__next_button}>
-                  <IconNextArrow />
-                </button>
-              </div>
-            )}
-            {showControls && (
-              <div className={styles.modal__dots}>
-                {images.map((_, index) => (
-                  <button
-                    key={index}
-                    className={`${styles.modal__dot} ${currentImageIndex === index ? styles.active : ''}`}
-                    onClick={() => handleDotClick(index)}
-                  />
-                ))}
-              </div>
-            )}
-            <IconClose onClick={handleConfirmation} className={styles.iconclose}>
+            <div className={styles.modal__dots}>
+              {images.map((_, index) => (
+                <button
+                  key={index}
+                  className={`${styles.modal__dot} ${currentImageIndex === index ? styles.active : ''}`}
+                  onClick={() => handleDotClick(index)}
+                />
+              ))}
+            </div>
+            <IconClose onClick={closeModal} className={styles.iconclose}>
               {buttonText}
             </IconClose>
           </div>
