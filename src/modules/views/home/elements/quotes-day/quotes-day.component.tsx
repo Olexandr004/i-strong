1c21b712a403f904d15a2d4e8b97dd854a122ed4
Olexandr004 <romanyyk2004@gmail.com>
2024-11-18 15:17:05 +0200

    is-162

--- src/modules/views/home/elements/quotes-day/quotes-day.component.tsx
+++ src/modules/views/home/elements/quotes-day/quotes-day.component.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image'
-
 import { FC, useMemo } from 'react'
 
 import { useGetQuery } from '@/api/daily-quote'
-import { ImageCapybara } from '@/shared/images'
 import { useUserStore } from '@/shared/stores'
 
 import styles from './quotes-day.module.scss'
@@ -17,22 +15,26 @@ export const QuotesDayComponent: FC<IQuotesDay> = () => {
   const { data, error, isLoading } = useGetQuery(token)
 
   return (
-    <section className={` ${styles.quotes}`}>
+    <section className={styles.quotes}>
       <h3 className={`text-3 ${styles.quotes__title}`}>Цитата дня</h3>
 
       <div className={styles.quotes__content}>
+        {/* Текст */}
         <div className={`text-4 ${styles.quotes__content_text}`}>
-          {' '}
           {isLoading
             ? 'Завантаження цитати...'
             : error
               ? 'Помилка завантаження'
               : (data as any).quote}
         </div>
+
+        {/* Изображение */}
         <div className={styles.quotes__img}>
           <Image
-            src={ImageCapybara}
+            src={'/image/capybara-chill.png'}
             alt={`Capybara`}
+            width={400}
+            height={200}
             style={{ objectFit: 'cover', width: '100%', height: '100%' }}
             priority
           />
